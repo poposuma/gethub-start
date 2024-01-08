be5634ea7709afaa11c7a91754b4aeefee7f235b
Anna Popova <poposuma@mail.ru>
2024-01-08 15:09:46 -0500

    my very first commit -changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React wiht me!
         </a>
       </header>
     </div>
